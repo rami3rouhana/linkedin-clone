4e4b56bf6b9893113a8a9077963a615757772380
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-10-16 11:16:02 +0300

    backend: add company schema

---
+++ linkedin-clone-backend/company/database/models/Company.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const CompanySchema = new Schema({
+    name: String,
+    email: String,
+    password: String,
+    salt: String,
+}, {
+    toJSON: {
+        transform(doc, ret) {
+            delete ret.password;
+            delete ret.salt;
+            delete ret.__v;
+        }
+    },
+    timestamps: true
+});
+
+module.exports = mongoose.model('company', CompanySchema);
