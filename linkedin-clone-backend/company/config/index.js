3538d3f20644467dd720aa20ac3b1813f09cf967
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-10-16 09:56:00 +0300

    backend: refactor config

--- linkedin-clone-backend/company/config/index.js
+++ linkedin-clone-backend/company/config/index.js
@@ -1,7 +1,7 @@
 const dotEnv = require("dotenv");
 
 if (process.env.NODE_ENV !== "prod") {
-  const configFile = `./.env.${process.env.NODE_ENV}`;
+  const configFile = `./${process.env.NODE_ENV}.env`;
   dotEnv.config({ path: configFile });
 } else {
   dotEnv.config();
