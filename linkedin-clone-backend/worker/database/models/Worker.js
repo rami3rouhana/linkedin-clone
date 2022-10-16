44f8b6a0ed32833189f91b6d62f33695e2aa949a
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-10-16 11:16:48 +0300

    backend: add worker schema

---
+++ linkedin-clone-backend/worker/database/models/Worker.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const WorkerSchema = new Schema({
+    name: String,
+    email: String,
+    password: String,
+    salt: String,
+    positions: String,
+    file: String,
+}, {
+    toJSON: {
+        transform(doc, ret) {
+            delete ret.password;
+            delete ret.salt;
+            delete ret.__v;
+        }
+    },
+    timestamps: true
+});
+
+module.exports = mongoose.model('worker', WorkerSchema);
