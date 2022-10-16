170ffde8ab8257947016c092b136e17a1977c777
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-10-16 08:47:20 +0300

    refactor project folders

---
+++ linkedin-clone-backend/company/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const StartServer = async() => {
+
+    const app = express();
+
+    app.listen(PORT, () => {
+        console.log(`listening to port ${PORT}`);
+    })
+    .on('error', (err) => {
+        console.log(err);
+        process.exit();
+    })
+
+}
+
+StartServer();
